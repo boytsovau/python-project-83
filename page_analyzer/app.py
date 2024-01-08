e39d3f7f18285f7f448db44578bc1f234570b497
boytsovau <alexbonch@gmail.com>
2024-01-08 17:26:26 +0300

    modified:   Makefile 	modified:   page_analyzer/app.py 	new file:   page_analyzer/build.sh 	new file:   page_analyzer/database.sql 	modified:   page_analyzer/templates/index.html 	modified:   poetry.lock 	modified:   pyproject.toml

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+import psycopg2
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/add_url', methods=['POST'])
+def add_url():
+    url = request.form['url']
+
+    if len(url) > 255:
+        flash('URL не должен превышать 255 символов', 'error')
+        return redirect(url_for('index'))
+
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO urls (name) VALUES (%s)", (url,))
+    conn.commit()
+    conn.close()
+
+    flash('URL успешно добавлен', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route('/urls')
+def show_urls():
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM urls ORDER BY id DESC")
+    urls = cur.fetchall()
+    conn.close()
+    return render_template('urls.html', urls=urls)
+
+
+@app.route('/urls/<int:url_id>')
+def show_url(url_id):
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
+    url = cur.fetchone()
+    conn.close()
+    return render_template('url.html', url=url)
