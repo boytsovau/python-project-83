32cfbeeab544397bcf14f4f8be0c43d3d76d005a
boytsovau <alexbonch@gmail.com>
2024-01-08 16:37:20 +0300

    modified:   Makefile 	modified:   page_analyzer/app.py 	new file:   page_analyzer/templates/index.html

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	poetry install
 
 dev:
-	poetry run flask --app page_analyzer:app run
+	poetry run flask --app page_analyzer.app:app run
 
 PORT ?= 8000
 start:
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,7 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
